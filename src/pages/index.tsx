ab8d0b83e5430764e2dd1325f72d4e7c4e399719
FeJustino <fwxjustino@gmail.com>
2025-08-05 23:08:02 -0300

    feat: add HeroSection component and SVG illustration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { HeroSection } from "@/components/hero-section/hero-section";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -12,11 +13,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} 
-      grid grid-rows-[20px_1fr_20px] items-center justify-items-center
-      min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]
-      bg-background`}>
-    </div>
+    <article className="flex flex-col">
+      <HeroSection />
+    </article>
   );
 }
